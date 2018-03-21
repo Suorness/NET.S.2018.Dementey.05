56b4022a15a85b3a8bb3fc40d4f03c433f3c9dd9
Vasya <inqire@gmail.com>
2018-03-21 21:57:53 +0300

    Add project files.

---
+++ Polynom/Polynomial.cs
@@ -0,0 +1,118 @@
+﻿namespace Polynom
+{
+    using System;
+
+    /// <summary>
+    /// This class describes a polynomial with certain methods of addition, subtraction and multiplication.
+    /// </summary>
+    public class Polynomial
+    {
+        #region constructor
+        public Polynomial(double[] coefficients)
+        {
+            Coefficients = coefficients ?? throw new ArgumentNullException(nameof(coefficients));
+        }
+        #endregion constructor
+
+        public double[] Coefficients { get; private set; }
+
+        #region public method
+
+        /// <summary>
+        /// This method adds two polynomials.
+        /// </summary>
+        /// <param name="firstPolynom">The first term. <see cref="Polynomial"/></param>
+        /// <param name="secondPolynom">The second term. <see cref="Polynomial"/></param>
+        /// <returns>The result of addition of two polynomials in the form of a new object. <see cref="Polynomial"/></returns>
+        public static Polynomial Add(Polynomial firstPolynom, Polynomial secondPolynom)
+        {
+            if (firstPolynom == null)
+            {
+                throw new ArgumentNullException(nameof(firstPolynom));
+            }
+
+            if (secondPolynom == null)
+            {
+                throw new ArgumentNullException(nameof(secondPolynom));
+            }
+
+            int lengthResultPolynom = Math.Max(firstPolynom.Coefficients.Length, secondPolynom.Coefficients.Length);
+
+            var resultPolynom = new double[lengthResultPolynom];
+            var firstCoefficients = new double[lengthResultPolynom];
+            var secondCoefficients = new double[lengthResultPolynom];
+
+            Array.Copy(firstPolynom.Coefficients, firstCoefficients, firstPolynom.Coefficients.Length);
+            Array.Copy(secondPolynom.Coefficients, secondCoefficients, secondPolynom.Coefficients.Length);
+
+            for (int i = 0; i < lengthResultPolynom; i++)
+            {
+                resultPolynom[i] = firstCoefficients[i] + secondCoefficients[i];
+            }
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// This method adds two polynomials.
+        /// </summary>
+        /// <param name="firstPolynom">The first term. <see cref="Polynomial"/></param>
+        /// <param name="secondPolynom">The second term. <see cref="Polynomial"/></param>
+        /// <returns>The result of addition of two polynomials in the form of a new object. <see cref="Polynomial"/></returns>
+        public static Polynomial operator +(Polynomial firstPolynom, Polynomial secondPolynom)
+        {
+            return Add(firstPolynom, secondPolynom);
+        }
+
+        /// <summary>
+        /// This method subtracts two polynomials.
+        /// </summary>
+        /// <param name="firstPolynom">The first term. <see cref="Polynomial"/></param>
+        /// <param name="secondPolynom">The second term. <see cref="Polynomial"/></param>
+        /// <returns>The result of subtracts of two polynomials in the form of a new object. <see cref="Polynomial"/></returns>
+        public static Polynomial Subtract(Polynomial firstPolynom, Polynomial secondPolynom)
+        {
+            if (firstPolynom == null)
+            {
+                throw new ArgumentNullException(nameof(firstPolynom));
+            }
+
+            if (secondPolynom == null)
+            {
+                throw new ArgumentNullException(nameof(secondPolynom));
+            }
+
+            int lengthResultPolynom = Math.Max(firstPolynom.Coefficients.Length, secondPolynom.Coefficients.Length);
+
+            var resultPolynom = new double[lengthResultPolynom];
+            var firstCoefficients = new double[lengthResultPolynom];
+            var secondCoefficients = new double[lengthResultPolynom];
+
+            Array.Copy(firstPolynom.Coefficients, firstCoefficients, firstPolynom.Coefficients.Length);
+            Array.Copy(secondPolynom.Coefficients, secondCoefficients, secondPolynom.Coefficients.Length);
+
+            for (int i = 0; i < lengthResultPolynom; i++)
+            {
+                resultPolynom[i] = firstCoefficients[i] - secondCoefficients[i];
+            }
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// This method subtracts two polynomials.
+        /// </summary>
+        /// <param name="firstPolynom">The first term. <see cref="Polynomial"/></param>
+        /// <param name="secondPolynom">The second term. <see cref="Polynomial"/></param>
+        /// <returns>The result of subtracts of two polynomials in the form of a new object. <see cref="Polynomial"/></returns>
+        public static Polynomial operator -(Polynomial firstPolynom, Polynomial secondPolynom)
+        {
+            return Subtract(firstPolynom, secondPolynom);
+        }
+
+        #endregion public method
+
+        #region private method
+        #endregion private method
+    }
+}
---
+++ PolynomTest/PolynomialTests.cs
@@ -0,0 +1,105 @@
+﻿namespace Polynom.Test
+{
+    using System;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Class for testing Polymomiac.
+    /// </summary>
+    public class PolymomiacTests
+    {
+        [TestCase(new double[] { 8, 16 })]
+        public void PolynomialCtorTest(double[] coefficients)
+        {
+            Assert.IsNotNull(new Polynomial(coefficients));
+        }
+
+        [TestCase]
+        public void PolynomialCtor_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Polynomial(null));
+        }
+
+        [TestCase]
+        public void Add_firstPolynomIsNull_ThrowsArgumentNullException()
+        {
+            Polynomial firstPolynim = new Polynomial(new double[] { 1 });
+            Polynomial secondPolynom = null;
+            Assert.Throws<ArgumentNullException>(() => Polynomial.Add(firstPolynim, secondPolynom));
+        }
+
+        [TestCase]
+        public void Add_secondPolynomIsNull_ThrowsArgumentNullException()
+        {
+            Polynomial firstPolynim = null;
+            Polynomial secondPolynom = new Polynomial(new double[] { 1 });
+            Assert.Throws<ArgumentNullException>(() => Polynomial.Add(firstPolynim, secondPolynom));
+        }
+
+        [TestCase(new double[] { 1 }, new double[] { 1 }, ExpectedResult = new double[] { 2 })]
+        [TestCase(new double[] { 0 }, new double[] { 1 }, ExpectedResult = new double[] { 1 })]
+        [TestCase(new double[] { 0 }, new double[] { 0 }, ExpectedResult = new double[] { 0 })]
+        [TestCase(new double[] { 1 }, new double[] { 0 }, ExpectedResult = new double[] { 1 })]
+        [TestCase(new double[] { 1, 2 }, new double[] { 1 }, ExpectedResult = new double[] { 2, 2 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 1 }, ExpectedResult = new double[] { 2, 2, 0, 0, 0, 8 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 1, 2, 0, 8 }, ExpectedResult = new double[] { 2, 4, 0, 8, 0, 8 })]
+        public double[] AddTest(double[] firstCoefficients, double[] secondCoefficients)
+        {
+            var firstPolynim = new Polynomial(firstCoefficients);
+            var secondPolynom = new Polynomial(secondCoefficients);
+
+            var result = Polynomial.Add(firstPolynim, secondPolynom);
+
+            return result.Coefficients;
+        }
+
+        [TestCase(new double[] { 1 }, new double[] { 1 }, ExpectedResult = new double[] { 2 })]
+        [TestCase(new double[] { 0 }, new double[] { 1 }, ExpectedResult = new double[] { 1 })]
+        [TestCase(new double[] { 0 }, new double[] { 0 }, ExpectedResult = new double[] { 0 })]
+        [TestCase(new double[] { 1 }, new double[] { 0 }, ExpectedResult = new double[] { 1 })]
+        [TestCase(new double[] { 1, 2 }, new double[] { 1 }, ExpectedResult = new double[] { 2, 2 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 1 }, ExpectedResult = new double[] { 2, 2, 0, 0, 0, 8 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 1, 2, 0, 8 }, ExpectedResult = new double[] { 2, 4, 0, 8, 0, 8 })]
+        public double[] OperatorPlusTest(double[] firstCoefficients, double[] secondCoefficients)
+        {
+            var firstPolynim = new Polynomial(firstCoefficients);
+            var secondPolynom = new Polynomial(secondCoefficients);
+
+            var result = firstPolynim + secondPolynom;
+
+            return result.Coefficients;
+        }
+
+        [TestCase(new double[] { 1 }, new double[] { 1 }, ExpectedResult = new double[] { 0 })]
+        [TestCase(new double[] { 2, 1 }, new double[] { 1 }, ExpectedResult = new double[] { 1, 1 })]
+        [TestCase(new double[] { 0 }, new double[] { 0 }, ExpectedResult = new double[] { 0 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 0, 0, 0, 0, 0, 8 }, ExpectedResult = new double[] { 1, 2, 0, 0, 0, 0 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 0, 0, 0, 0, 0, 0 }, ExpectedResult = new double[] { 1, 2, 0, 0, 0, 8 })]
+        [TestCase(new double[] { 0 }, new double[] { 0 }, ExpectedResult = new double[] { 0 })]
+        public double[] Subtract(double[] firstCoefficients, double[] secondCoefficients)
+        {
+            var firstPolynim = new Polynomial(firstCoefficients);
+            var secondPolynom = new Polynomial(secondCoefficients);
+
+            var result = Polynomial.Subtract(firstPolynim, secondPolynom);
+
+            return result.Coefficients;
+        }
+
+        [TestCase(new double[] { 1 }, new double[] { 1 }, ExpectedResult = new double[] { 0 })]
+        [TestCase(new double[] { 2, 1 }, new double[] { 1 }, ExpectedResult = new double[] { 1, 1 })]
+        [TestCase(new double[] { 0 }, new double[] { 0 }, ExpectedResult = new double[] { 0 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 0, 0, 0, 0, 0, 8 }, ExpectedResult = new double[] { 1, 2, 0, 0, 0, 0 })]
+        [TestCase(new double[] { 1, 2, 0, 0, 0, 8 }, new double[] { 0, 0, 0, 0, 0, 0 }, ExpectedResult = new double[] { 1, 2, 0, 0, 0, 8 })]
+        [TestCase(new double[] { 0 }, new double[] { 0 }, ExpectedResult = new double[] { 0 })]
+        public double[] OperatorSubtractTest(double[] firstCoefficients, double[] secondCoefficients)
+        {
+            var firstPolynim = new Polynomial(firstCoefficients);
+            var secondPolynom = new Polynomial(secondCoefficients);
+
+            var result = firstPolynim - secondPolynom;
+
+            return result.Coefficients;
+        }
+    }
+}
